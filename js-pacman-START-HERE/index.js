a82384feab8d4a9694b0e85a386010b280c6b04f
Dino <dinopranjic92@gmail.com>
2023-01-15 15:24:32 -0500

    Add collision detection and powerpill status

--- js-pacman-START-HERE/index.js
+++ js-pacman-START-HERE/index.js
@@ -23,16 +23,80 @@ let powerPillActive = false;
 let powerPillTimer = null;
 
 function gameOver(pacman, grid) {
+  document.removeEventListener('keydown', e =>
+    pacman.handleKeyInput(e, gameBoard.objectExist));
+
+    gameBoard.showGameStatus(gameWin);
+
+    clearInterval(timer);
+
+    startButton.classList.remove('hide');
+
 
 }
 
 function checkCollision(pacman, ghosts) {
+  const collidedGhost = ghosts.find( ghost => pacman.pos === ghost.pos);
 
+  if (collidedGhost) {
+    if (pacman.powerPill) {
+      gameBoard.removeObject(collidedGhost.pos, [
+        OBJECT_TYPE.GHOST,
+        OBJECT_TYPE.SCARED,
+        collidedGhost.name
+      ]);
+      collidedGhost.pos = collidedGhost.startPos;
+      score += 100;
+    } else {
+      gameBoard.removeObject(pacman.pos, [OBJECT_TYPE.PACMAN]);
+      gameBoard.rotateDiv(pacman.pos, 0);
+      gameOver(pacman, gameGrid);
+    }
+  }
 }
 
 function gameLoop(pacman, ghosts) {
   gameBoard.moveCharacter(pacman);
+  checkCollision(pacman, ghosts);
+
   ghosts.forEach((ghost) => gameBoard.moveCharacter(ghost));
+  checkCollision(pacman, ghosts);
+
+  // Check if pacman eats a dot
+  if (gameBoard.objectExist(pacman.pos, OBJECT_TYPE.DOT)) {
+    gameBoard.removeObject(pacman.pos, [OBJECT_TYPE.DOT]);
+    gameBoard.dotCount--;
+    score += 10;
+  }
+
+  // Check if pacman eats a powerpill
+  if (gameBoard.objectExist(pacman.pos, OBJECT_TYPE.PILL)) {
+    gameBoard.removeObject(pacman.pos, [OBJECT_TYPE.PILL]);
+
+    pacman.powerPill= true;
+    score += 50;
+
+    clearTimeout(powerPillTimer);
+    powerPillTimer = setTimeout(
+      () => (pacman.powerPill = false),
+      POWER_PILL_TIME
+    );
+  }
+
+  // Change ghosts to scared mode
+  if (pacman.powerPill !== powerPillActive) {
+    powerPillActive = pacman.powerPill;
+    ghosts.forEach(ghost => ghost.isScared = pacman.powerPill);
+  }
+
+  // Check if all dots have been eaten
+  if (gameBoard.dotCount === 0) {
+    gameWin = true;
+    gameOver(pacman, gameGrid)
+  }
+
+  // Show score count
+  scoreTable.innerHTML = score;
 }
 
 function startGame() {
